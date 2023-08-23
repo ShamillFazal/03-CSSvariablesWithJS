a7a79e3e5e8d550303e9a2d6e6208595a86f031f
unknown <shamy_pw4@hotmail.co.uk>
2023-08-23 21:40:57 +0100

    cleaned up and commented code

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-
+// Select all input elements with class "controls"
 const inputs = document.querySelectorAll('.controls input');
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
-inputs.forEach(input => input.addEventListener("mousemove", handleUpdate))
+// Add a 'change' event listener to each input element
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
 
+// Add a 'mousemove' event listener to each input element
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
+// Function that handles the updates based on input changes or mouse movement
 function handleUpdate() {
-    const suffix = this.dataset.sizing || ''
-    console.log(this.name);
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
-}
+    // Retrieve the 'data-sizing' attribute value from the input, or an empty string if it's not present
+    const suffix = this.dataset.sizing || '';
 
+    // Update a CSS variable (custom property) with the calculated value from the input
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
